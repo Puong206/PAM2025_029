208beddf22acf1cae627d5a8bbbb9748236d59b9
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 05:54:45 +0700

    Menginisialisasi `AppContainer` pada kelas `RailSensusApp`
    Menambahkan variabel `container` dengan tipe `AppContainer` dan menginisialisasinya menggunakan `ContainerApp()` di dalam metode `onCreate`. Langkah ini bertujuan untuk menyiapkan dependensi aplikasi saat aplikasi dijalankan.

--- app/src/main/java/com/example/railsensus/RailSensusApp.kt
+++ app/src/main/java/com/example/railsensus/RailSensusApp.kt
@@ -1,7 +1,16 @@
 package com.example.railsensus
 
 import android.app.Application
+import com.example.railsensus.repositori.AppContainer
+import com.example.railsensus.repositori.ContainerApp
 
 class RailSensusApp : Application() {
+    
+    lateinit var container: AppContainer
 
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerApp()
+
+    }
 }
