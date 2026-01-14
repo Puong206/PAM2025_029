c7409af7bbff02ce654f4713113ec6abadf75c7a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 05:59:40 +0700

    Menambahkan antarmuka `AppContainer` dan kelas `ContainerApp`
    Menambahkan `AppContainer` sebagai interface untuk injeksi dependensi dan kelas `ContainerApp` yang mengimplementasikannya di dalam berkas `ContainerApp.kt`. Modifikasi ini juga menyertakan impor untuk `ServiceApi`.

--- app/src/main/java/com/example/railsensus/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/railsensus/repositori/ContainerApp.kt
@@ -1,2 +1,11 @@
 package com.example.railsensus.repositori
 
+import com.example.railsensus.apiservice.ServiceApi
+
+interface AppContainer{
+    val serviceApi: ServiceApi
+}
+
+class ContainerApp: AppContainer{
+
+}
