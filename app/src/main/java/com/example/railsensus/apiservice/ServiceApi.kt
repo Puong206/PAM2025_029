99453f43f52a17196e96dc09b9e9809c829dc1a0
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 04:57:38 +0700

    Menambahkan endpoint autentikasi pada `ServiceApi.kt`
    Menambahkan fungsi `register`, `login`, dan `getProfile` ke dalam interface `ServiceApi` untuk menangani permintaan API terkait autentikasi menggunakan Retrofit.

--- app/src/main/java/com/example/railsensus/apiservice/ServiceApi.kt
+++ app/src/main/java/com/example/railsensus/apiservice/ServiceApi.kt
@@ -1,5 +1,27 @@
 package com.example.railsensus.apiservice
 
-interface ServiceApi {
+import com.example.railsensus.modeldata.LoginRequest
+import com.example.railsensus.modeldata.LoginResponse
+import com.example.railsensus.modeldata.RegisterRequest
+import com.example.railsensus.modeldata.RegisterResponse
+import com.example.railsensus.modeldata.UserProfile
+import retrofit2.Response
+import retrofit2.http.*
 
+interface ServiceApi {
+    // Auth
+    @POST("api/auth/register")
+    suspend fun register(
+        @Body request: RegisterRequest
+    ): Response<RegisterResponse>
+
+    @POST("api/auth/login")
+    suspend fun login(
+        @Body request: LoginRequest
+    ): Response<LoginResponse>
+
+    @GET("api/auth/me")
+    suspend fun getProfile(
+        @Header("x-acces-token") token: String
+    ): Response<UserProfile>
 }
