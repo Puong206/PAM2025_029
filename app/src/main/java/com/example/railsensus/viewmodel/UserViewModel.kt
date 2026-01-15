839645d52a9d9c3279dd94042d2711c171d449d4
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 01:54:38 +0700

    Menambahkan implementasi `UserViewModel`
    Menambahkan kelas `UserViewModel` untuk mengelola data pengguna. Implementasi ini mencakup `StateFlow` untuk daftar pengguna (`userList`), detail pengguna yang dipilih (`selectedUser`), statistik pengguna (`statistik`), serta status pemuatan (`isLoading`) dan pesan kesalahan (`errorMessage`).

--- app/src/main/java/com/example/railsensus/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/UserViewModel.kt
@@ -1,2 +1,32 @@
 package com.example.railsensus.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.railsensus.modeldata.StatistikUser
+import com.example.railsensus.modeldata.UserManagement
+import com.example.railsensus.repositori.RepositoriRailSensus
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class UserViewModel(
+    private val repositori: RepositoriRailSensus
+): ViewModel() {
+    // list users
+    private val _userList = MutableStateFlow<List<UserManagement>>(emptyList())
+    val userList: StateFlow<List<UserManagement>> = _userList.asStateFlow()
+
+    // selected user for detail
+    private val _selectedUser = MutableStateFlow<UserManagement?>(null)
+    val selectedUser: StateFlow<UserManagement?> = _selectedUser.asStateFlow()
+
+    // statistik
+    private val _statistik = MutableStateFlow<StatistikUser?>(null)
+    val statistik: StateFlow<StatistikUser?> = _statistik.asStateFlow()
+
+    // loading & Error
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage.asStateFlow()
+}
