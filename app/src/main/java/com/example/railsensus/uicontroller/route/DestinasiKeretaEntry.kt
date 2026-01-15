44bd53083b9ddc378c8880c9ae0c5983cc2bf700
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 03:00:01 +0700

    Menambahkan objek navigasi `DestinasiKeretaEntry`
    Menambahkan file baru `DestinasiKeretaEntry.kt` yang menentukan rute navigasi `entry_kereta` dan judul halaman "Tambah Kereta" untuk fitur entri data kereta.

---
+++ app/src/main/java/com/example/railsensus/uicontroller/route/DestinasiKeretaEntry.kt
@@ -0,0 +1,6 @@
+package com.example.railsensus.uicontroller.route
+
+object DestinasiKeretaEntry : DestinasiNavigasi {
+    override val route = "entry_kereta"
+    override val titleRes = "Tambah Kereta"
+}
