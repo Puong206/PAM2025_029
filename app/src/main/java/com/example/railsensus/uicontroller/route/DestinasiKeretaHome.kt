2bd2da7476abdd3185e1f8929f33ac64a6b65f31
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 03:00:12 +0700

    Menambahkan objek navigasi `DestinasiKeretaHome`
    Menambahkan file baru `DestinasiKeretaHome.kt` yang mengimplementasikan antarmuka `DestinasiNavigasi` dengan rute `"kereta_home"` dan judul `"Daftar Kereta"`.

---
+++ app/src/main/java/com/example/railsensus/uicontroller/route/DestinasiKeretaHome.kt
@@ -0,0 +1,6 @@
+package com.example.railsensus.uicontroller.route
+
+object DestinasiKeretaHome : DestinasiNavigasi {
+    override val route = "kereta_home"
+    override val titleRes = "Daftar Kereta"
+}
