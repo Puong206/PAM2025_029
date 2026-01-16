3748c2357da06423729aaf4b729a53b5746e24c7
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-17 02:02:22 +0700

    Menghapus berkas `ViewModelProvider.kt`
    Menghapus file `ViewModelProvider.kt` yang berisi fungsi ekstensi `railSensusApplication()` untuk `CreationExtras`. Berkas ini sebelumnya digunakan untuk mempermudah akses ke instans aplikasi di dalam `ViewModelProvider`.

--- app/src/main/java/com/example/railsensus/viewmodel/provider/ViewModelProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.example.railsensus.viewmodel.provider
-
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
-import com.example.railsensus.RailSensusApp
-
-fun CreationExtras.railSensusApplication(): RailSensusApp =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RailSensusApp)
-
